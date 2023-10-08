1e0f5036d96a254ad09f7e6cfc04dc70dd1e4216
Marcin Kurek <3824342+marcinkurek@users.noreply.github.com>
2023-10-08 02:34:34 +0000

    Use OmniSharp

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo touch /usr/share/dotnet/sdk/$(dotnet --version)/EnableWorkloadResolver.sentinel
-sudo dotnet workload restore
+sudo dotnet workload restore --project Dionysus.Wasm/Dionysus.Wasm.csproj
