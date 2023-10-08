4ac86b902c64c9a13c9e837c74b4cf26ca14ba83
Marcin Kurek <3824342+marcinkurek@users.noreply.github.com>
2023-10-08 01:48:28 +0000

    Add postAttachCommand.sh

---
+++ .devcontainer/postAttachCommand.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo ""
+echo "Welcome to Dionysus dev container."
+echo ""
+echo "Notes:"
+echo "- Follow the instructions in solution-config.props.sample and uncomment line 16."
+echo "- To run in a GitHub Codespace, set port 5000 visibility to Public. Be advised that this enables outside anonymous access."
+echo ""
+echo "Useful commands:"
+echo "- build: dotnet build Dionysus.Wasm"
+echo "- run:   dotnet run --project Dionysus.Wasm"
+echo ""
